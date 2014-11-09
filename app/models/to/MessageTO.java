c328a6374758a68cfcbc5eaa0c4f117499fc53ca
gorans90 <simic.goran90@gmail.com>
2014-11-09 22:53:43 +0100

    Added messageTO

---
+++ app/models/to/MessageTO.java
@@ -0,0 +1,20 @@
+package models.to;
+
+import java.util.Date;
+
+import models.User;
+
+public class MessageTO {
+
+	public String title;
+
+	public String messageBody;
+
+	public String usernameFrom;
+
+	public String usernameTo;
+
+	public String date;
+
+	public long inboxid;
+}

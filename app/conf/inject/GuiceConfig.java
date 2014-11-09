0213b81c1b5cdf42570fa4a7b2d030d00bde82ed
stefmil <moonwalker13@gmail.com>
2014-11-09 17:24:54 +0100

    Commit

--- app/conf/inject/GuiceConfig.java
+++ app/conf/inject/GuiceConfig.java
@@ -1,9 +1,15 @@
 package conf.inject;
 
+import models.Address;
+import models.Place;
 import play.modules.guice.GuiceSupport;
+import service.AddressService;
 import service.AdsService;
+import service.PlaceService;
 import service.UserService;
+import service.impl.AddressServiceImpl;
 import service.impl.AdsServiceImpl;
+import service.impl.PlaceServiceImpl;
 import service.impl.UserServiceImpl;
 
 import com.google.inject.AbstractModule;
@@ -11,8 +17,10 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 
+import controllers.AddressController;
 import controllers.AdsController;
 import controllers.Application;
+import controllers.PlaceController;
 import controllers.UserController;
 
 public class GuiceConfig extends GuiceSupport {
@@ -24,9 +32,13 @@ public class GuiceConfig extends GuiceSupport {
 			requestStaticInjection(Application.class);
 			requestStaticInjection(UserController.class);
 			requestStaticInjection(AdsController.class);
+			requestStaticInjection(AddressController.class);
+			requestStaticInjection(PlaceController.class);
 			
 			bind(UserService.class).to(UserServiceImpl.class);
 			bind(AdsService.class).to(AdsServiceImpl.class);
+			bind(AddressService.class).to(AddressServiceImpl.class);
+			bind(PlaceService.class).to(PlaceServiceImpl.class);
 		}
 	};
 //
